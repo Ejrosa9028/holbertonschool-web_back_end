179a516aa30ab060c9ceb1c56d90a74a99995872
Ejrosa9028 <132318096+Ejrosa9028@users.noreply.github.com>
2025-09-08 06:59:32 -0400

    1. Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      // No se deben cambiar los valores de task y task2
+    }
+  
+    return [task, task2];
+  }
