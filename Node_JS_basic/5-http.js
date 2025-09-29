d5c59fbdb8188688f32d8519e65d1b7e7734d62e
Ejrosa9028 <132318096+Ejrosa9028@users.noreply.github.com>
2025-09-29 01:03:37 -0400

    fix code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,25 +2,28 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const path = process.argv[2];
+
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
-    countStudents(process.argv[2])
+
+    countStudents(path)
       .then((output) => {
         res.end(output);
       })
-      .catch((error) => {
-        res.end(error.message);
+      .catch((err) => {
+        res.end(err.message);
       });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
   }
 });
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is listening on port 1245');
+});
 
 module.exports = app;
